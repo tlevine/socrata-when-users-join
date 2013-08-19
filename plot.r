1280d2ffbc35aa78e78ad54fa221888ad530b7c1
Thomas Levine <occurrence@thomaslevine.com>
2013-08-19 14:32:28 -0700

    a simple plot

---
+++ plot.r
@@ -0,0 +1,18 @@
+library(sqldf)
+library(ggplot2)
+
+if (!('series' %in% ls())) {
+  series <- sqldf('SELECT * FROM series;', dbname = 'metrics.db')
+  names(series) <- sapply(names(series), function(x) { gsub('-', '.', x)})
+}
+
+if (!('series.zero' %in% ls())) {
+  series.zero <- series
+  series.zero[is.na(series)] <- 0
+  series.zero[1:nrow(series),] <- as.data.frame(series.zero)
+}
+
+p <- ggplot(series.zero) +
+  aes(x = date, y = users.created, group = portal, color = portal) +
+  scale_x_date('Date') + scale_y_continuous('Users created today') +
+  geom_line()
