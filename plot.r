64a2ca4ed6c7a9b1e09028313f7ed13a0da5c109
Thomas Levine <occurrence@thomaslevine.com>
2013-08-19 14:38:53 -0700

    notes

--- plot.r
+++ plot.r
@@ -2,7 +2,7 @@ library(sqldf)
 library(ggplot2)
 
 if (!('series' %in% ls())) {
-  series <- sqldf('SELECT * FROM series;', dbname = 'metrics.db')
+  series <- sqldf('SELECT * FROM "series" WHERE "date" >= \'2010-01-01\' ;', dbname = 'metrics.db')
   names(series) <- sapply(names(series), function(x) { gsub('-', '.', x)})
   series$date <- as.Date(series$date)
 }
@@ -15,5 +15,17 @@ if (!('series.zero' %in% ls())) {
 
 p <- ggplot(series.zero) +
   aes(x = date, y = users.created, group = portal, color = portal) +
-  scale_x_date('Date') + scale_y_continuous('Users created today') +
+  scale_x_date('Date') + scale_y_log10('Users created today') +
+  labs(title = 'Which portals aquired users when?') +
   geom_line()
+
+# Standardize by portal
+
+
+# Facet
+
+
+# Day of week
+
+
+# Notable events, like hackathons, legislation
