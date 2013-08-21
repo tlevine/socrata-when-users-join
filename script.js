19296bfe5651a1fca9989d594c3b84608197306c
Thomas Levine <occurrence@thomaslevine.com>
2013-08-21 02:58:51 -0700

    oaeu

--- script.js
+++ script.js
@@ -5,19 +5,9 @@
     }
   })
   
-  function separatePortals(full_table){
-    var result = {}
-    while (full_table.length > 0) {
-      var row = full_table.pop()
-      if (!(row.portal in result)) result[row.portal] = []
-      result[row.portal].push(row)
-    }
-    return result
-  }
-
   function makePlot(table) {
-    var portals = separatePortals(table)
-
+    table = table.map(convertTypes)
+    
     // http://bl.ocks.org/benjchristensen/2579599
 
     // define dimensions of graph
@@ -25,11 +15,20 @@
     var w = 840 - m[1] - m[3]; // width
     var h = 480 - m[0] - m[2]; // height
 
-    var x = d3.scale.linear().domain([0, portals['explore.data.gov'].length]).range([0, w]);
-    
-  }
+    var x = d3.time.scale().domain([minDate, maxDate]).range([0, w]).nice(d3.time.day)
+    // var y = d3.scale.linear().domain([0, 1000]).range([h, 0]);
 
-  function chooseVariable(variable) {
-    var y = d3.scale.linear().domain([0, ]).range([h, 0]);
+    function convertTypes(row){
+      var date = new Date()
+      var date_components = date.split('-')
+      date.setYear(date_components[0])
+      date.setMonth(date_components[1])
+      date.setDate(date_components[2])
+      date.setHours(0)
+      date.setMinutes(0)
+      date.setSeconds(0)
+      row.date = date
+      return row
+    }
   }
 })()
