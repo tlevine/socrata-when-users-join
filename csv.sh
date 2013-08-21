e2024ce64fc4163c250830e61969ba9d259bc24e
Thomas Levine <occurrence@thomaslevine.com>
2013-08-21 02:32:58 -0700

    order

--- csv.sh
+++ csv.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-sqlite3 -csv -header metrics.db "SELECT * FROM series where date >= '2010-06-01'" |
+sqlite3 -csv -header metrics.db "SELECT * FROM series where date >= '2010-06-01' ORDER BY date" | 
   sed 's/,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,$/,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,/' > metrics.csv
