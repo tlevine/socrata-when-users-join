6545dd027250d4150588852f6ec38269284854e6
Thomas Levine <occurrence@thomaslevine.com>
2013-08-21 02:50:38 -0700

    oeu

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 (function(){
-  window.onload = function(){
-    d3.csv('series.csv', makePlot)
-  }
+  d3.csv('series.csv', function(data) {
+    window.onload = function() {
+      makePlot(data)
+    }
+  })
   
   function separatePortals(full_table){
     var result = {}
@@ -15,6 +17,12 @@
 
   function makePlot(table) {
     var portals = separatePortals(table)
-  }
 
+    // define dimensions of graph
+    var m = [80, 80, 80, 80]; // margins
+    var w = 840 - m[1] - m[3]; // width
+    var h = 480 - m[0] - m[2]; // height
+
+    
+  }
 })()
