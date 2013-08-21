424685d9f012ee494559962b3c59a71584ee44ee
Thomas Levine <occurrence@thomaslevine.com>
2013-08-21 02:52:28 -0700

    oeu

--- script.js
+++ script.js
@@ -18,11 +18,18 @@
   function makePlot(table) {
     var portals = separatePortals(table)
 
+    // http://bl.ocks.org/benjchristensen/2579599
+
     // define dimensions of graph
     var m = [80, 80, 80, 80]; // margins
     var w = 840 - m[1] - m[3]; // width
     var h = 480 - m[0] - m[2]; // height
 
+    var x = d3.scale.linear().domain([0, portals['explore.data.gov'].length]).range([0, w]);
     
+  }
+
+  function chooseVariable(variable) {
+    var y = d3.scale.linear().domain([0, ]).range([h, 0]);
   }
 })()
