aa60f5f21f7c6ab03bd35cb403e1cd31e299cc6f
Thomas Levine <occurrence@thomaslevine.com>
2013-09-28 20:19:56 -0700

    aoeu

--- plot.r
+++ plot.r
@@ -11,9 +11,11 @@ if (!('series.zero' %in% ls())) {
   series.zero <- series
   series.zero[is.na(series)] <- 0
   series.zero[1:nrow(series),] <- as.data.frame(series.zero)
+  series.zero$day.of.week <- factor(strftime(series.zero$date, '%a'),
+    c('Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'))
 }
 
-p <- ggplot(series.zero) +
+p1 <- ggplot(series.zero) +
   aes(x = date, y = users.created, group = portal, color = portal) +
   scale_x_date('Date') + scale_y_log10('Users created today') +
   labs(title = 'Which portals aquired users when?') +
@@ -23,9 +25,13 @@ p <- ggplot(series.zero) +
 
 
 # Facet
+p3 <- p1 + facet_wrap(~ portal)
 
 
 # Day of week
-
+p4 <- ggplot(series.zero) + aes(x = day.of.week) + geom_histogram() +
+  scale_y_continuous('Number of users added on this day of the week') +
+  scale_x_discrete('Day of the week') +
+  ggtitle('Day of the week doesn\'t tell us much.')
 
 # Notable events, like hackathons, legislation
